208a984e60e19693471f11f06cc6229b9957717d
Garoven <jaceksobczak803@gmail.com>
2023-08-16 20:26:16 +0200

    added lib.rs to clean main file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-mod day_1;
-mod day_2;
-mod day_3;
-
 #[derive(clap::Parser, Debug)]
 struct Args {
     #[arg(short, long)]
@@ -15,9 +11,9 @@ fn main() {
     let input = std::fs::read_to_string(args.input).unwrap();
 
     match args.day {
-        1 => day_1::combined(&input),
-        2 => day_2::combined(&input),
-        3 => day_3::combined(&input),
+        1 => advent_of_code_2022::day_1::combined(&input),
+        2 => advent_of_code_2022::day_2::combined(&input),
+        3 => advent_of_code_2022::day_3::combined(&input),
         4..=25 => unimplemented!(),
         _ => panic!("Select day between 1 and 25"),
     }
